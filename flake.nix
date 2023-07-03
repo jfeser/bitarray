3bf8977c253f7de2a91b97482576ba8f36882a09
Jack Feser <feser@mit.edu>
2023-07-03 17:53:06 -0400

    update nix

--- flake.nix
+++ flake.nix
@@ -1,42 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, flake-utils, nixpkgs }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay.${system} ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        ocamlPkgs = pkgs.ocaml-ng.ocamlPackages;
+        ispc = if system == "aarch64-darwin" then
+          pkgs.callPackage ./nix/ispc-darwin.nix { }
+        else
+          pkgs.ispc;
+
+        bitarray = ocamlPkgs.buildDunePackage {
+          pname = "bitarray";
+          version = "0.1";
+          useDune3 = true;
+          minimalOCamlVersion = "4.13";
+          nativeBuildInputs = [ ispc ];
+          propagatedBuildInputs = with ocamlPkgs; [ base fmt ppx_jane ];
+          src = ./.;
         };
       in {
-        overlay = self: super:
-          let
-            ispc = if system == "aarch64-darwin" then
-              super.callPackage ./nix/ispc-darwin.nix { }
-            else
-              self.ispc;
-            gcc = self.gcc;
-          in {
-            ocamlPackages = super.ocamlPackages.overrideScope' (self: super: {
-              bitarray = super.buildDunePackage {
-                pname = "bitarray";
-                version = "0.1";
-                useDune3 = true;
-                minimalOCamlVersion = "4.13";
-                nativeBuildInputs =
-                  [ self.base_quickcheck self.core self.core_bench gcc ispc ];
-                propagatedBuildInputs = [ self.base self.fmt ];
-                src = ./.;
-              };
-            });
-          };
-        defaultPackage = pkgs.ocamlPackages.bitarray;
+        defaultPackage = bitarray;
         devShell = pkgs.mkShell {
-          nativeBuildInputs =
-            [ pkgs.ocamlformat pkgs.opam pkgs.ocamlPackages.ocaml-lsp ];
-          inputsFrom = [ pkgs.ocamlPackages.bitarray ];
+          nativeBuildInputs = [
+            pkgs.ocamlformat
+            pkgs.opam
+            pkgs.ocamlPackages.ocaml-lsp
+            pkgs.ocamlPackages.core
+            pkgs.ocamlPackages.base_quickcheck
+          ];
+          inputsFrom = [ bitarray ];
         };
       });
 }
